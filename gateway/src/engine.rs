6910f81b0134e63dc8aa327d3b34642f772bc1ba
Matthew <15010780+Matthew0822@users.noreply.github.com>
2012-11-14 16:15:00 +0000

    build: audit

--- gateway/src/engine.rs
+++ gateway/src/engine.rs
@@ -262,3 +262,16 @@ redaction_mask = \"***\"
             r#"{"method":"tools/call","params":{"name":"read_file","arguments":{"path":"/x","auth_token":"s3cr3t"}}}"#,
             &p,
             &mut rl,
+            1,
+            0,
+        );
+        let fwd = String::from_utf8(out.forward.unwrap()).unwrap();
+        assert!(fwd.contains(r#""auth_token":"***""#), "got: {fwd}");
+        assert_eq!(out.event.redacted, vec!["auth_token".to_string()]);
+    }
+
+    #[test]
+    fn size_limit_drops() {
+        let mut p = engine_policy();
+        p.max_bytes = 10;
+        let mut rl = RateLimiter::new(p.rate_limit, p.rate_window_ms);
