6a8c03f3437efcbe121a4927e06f2528ceeaba52
Matthew <15010780+Matthew0822@users.noreply.github.com>
2013-02-13 09:48:00 +0000

    test: cover reports

--- gateway/src/lib.rs
+++ gateway/src/lib.rs
@@ -7,3 +7,4 @@
 //!   * [`audit`]     — structured, one-line-per-event JSON audit records;
 //!   * [`engine`]    — the pure per-message decision pipeline.
 //!
+//! Everything here depends solely on the Rust standard library.
