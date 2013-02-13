eb12521e4c03807116fab2773da3c231b82c4824
Matthew <15010780+Matthew0822@users.noreply.github.com>
2013-02-13 09:32:00 +0000

    style: clean up console

--- gateway/src/lib.rs
+++ gateway/src/lib.rs
@@ -5,3 +5,4 @@
 //!   * [`policy`]    — the line-oriented policy format, matcher and rate limiter;
 //!   * [`redact`]    — argument-value redaction that preserves message bytes;
 //!   * [`audit`]     — structured, one-line-per-event JSON audit records;
+//!   * [`engine`]    — the pure per-message decision pipeline.
