8730432eb5b59b4333de56364aba9d47ddba4a9b
Matthew <15010780+Matthew0822@users.noreply.github.com>
2013-02-13 09:15:00 +0000

    refactor: tidy cli

--- gateway/src/lib.rs
+++ gateway/src/lib.rs
@@ -3,3 +3,4 @@
 //! The binary in `main.rs` is a thin I/O shell around these modules:
 //!   * [`json_scan`] — the honest, single-pass JSON field extractor;
 //!   * [`policy`]    — the line-oriented policy format, matcher and rate limiter;
+//!   * [`redact`]    — argument-value redaction that preserves message bytes;
