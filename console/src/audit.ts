ef458a1cd87c8219ca8d731d7caad4fc9efc10dd
Matthew <15010780+Matthew0822@users.noreply.github.com>
2016-03-30 14:01:00 +0000

    style: clean up json-scan

--- console/src/audit.ts
+++ console/src/audit.ts
@@ -99,3 +99,15 @@ function asStringArray(o: Record<string, unknown>, key: string): string[] {
 function asDecision(o: Record<string, unknown>, key: string): Decision {
   const v = asString(o, key);
   if (!(DECISIONS as readonly string[]).includes(v)) {
+    throw new Error(`field '${key}' has invalid decision '${v}'`);
+  }
+  return v as Decision;
+}
+
+/** Parse a single trimmed line into an event, summary, or error record. */
+export function parseLine(raw: string, lineNo: number): ParsedLine {
+  let value: unknown;
+  try {
+    value = JSON.parse(raw);
+  } catch (err) {
+    return {
