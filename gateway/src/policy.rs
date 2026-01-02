59d438865f960c52314faa8ebcf37a38567a43a8
Matthew <15010780+Matthew0822@users.noreply.github.com>
2026-01-02 15:27:00 +0000

    test: cover policy

--- gateway/src/policy.rs
+++ gateway/src/policy.rs
@@ -451,3 +451,6 @@ redaction_mask = \"[hidden]\"
         let p = Policy::parse("redaction_mask = \"a#b\" # trailing\n").unwrap();
         assert_eq!(p.redaction_mask, "a#b");
     }
+}
+
+# draft note 1618
